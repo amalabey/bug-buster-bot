967a25ea01eb5215805f7dff164dc2f739c0f6b8
Amal Abeygunawardana <amalabey@gmail.com>
2023-07-10 17:58:05 +1000

    PR decoration works but all done on one file

--- app/azure_devops/azure_repo_pr_decorator_service.py
+++ app/azure_devops/azure_repo_pr_decorator_service.py
@@ -1,6 +1,6 @@
 import json
 from app.azure_devops.azure_devops_client import AzureDevOpsClient
-from app.models import ReviewCommentCollection
+from app.models import ReviewComment, ReviewCommentCollection
 from app.pr_decorator_service import PullRequestDecoratorService
 
 
@@ -12,27 +12,43 @@ class AzureRepoPullRequestDecoratorService(PullRequestDecoratorService):
         self.project_name = project_name
         self.repo_name = repo_name if repo_name is not None else project_name
 
-    def _get_comment_payload(self, file_path: str, line_num: int, comments: ReviewCommentCollection):
-        comments_list = list()
-        for comment in comments.comments:
-            comment_text = f"{comment.feedback} \n ```\n{comment.example}\n```"
-            comment_payload = {
-                                    "parentCommentId": 0,
-                                    "content": comment_text,
-                                    "threadContext": {
-                                        "filePath": file_path,
-                                        "rightFileStartLine": line_num,
-                                        "rightFileEndLine": line_num
-                                    }
-                                }
-            comments_list.append(comment_payload)
+    def _get_comment_payload(self, file_path: str, line_num: int, comment: ReviewComment):
+        comment_text = f"{comment.feedback} \n ```\n{comment.example}\n```"
         payload = json.dumps({
-                        "comments": comments_list
+                        "comments": [
+                            {
+                                "parentCommentId": 0,
+                                "content": comment_text,
+                                "commentType": 1
+                            }
+                        ],
+                        "status": 1,
+                        "threadContext": {
+                            "filePath": file_path,
+                            "leftFileEnd": None,
+                            "leftFileStart": None,
+                            "rightFileEnd": {
+                                "line": line_num,
+                                "offset": 1
+                            },
+                            "rightFileStart": {
+                                "line": line_num,
+                                "offset": 1
+                            }
+                        },
+                        "pullRequestThreadContext": {
+                            "changeTrackingId": 1,
+                            "iterationContext": {
+                                "firstComparingIteration": 1,
+                                "secondComparingIteration": 2
+                            }
+                        }
                     })
         return payload
 
     def annotate(self, pr_id: str, file_path: str, line_num: int, comments: ReviewCommentCollection):
         api_url = f"https://dev.azure.com/{self.org}/{self.project_name}/_apis/git/repositories/{self.repo_name}/pullRequests/{pr_id}/threads?api-version=6.1-preview.1"
-        payload = self._get_comment_payload(file_path, line_num, comments)
         api = AzureDevOpsClient()
-        api.send_api_request(api_url, "POST", data=payload)
+        for comment in comments.comments:
+            payload = self._get_comment_payload(file_path, line_num, comment)
+            api.send_api_request(api_url, "POST", data=payload)
